95295437e984891e141ba58299fa78079f70935a
musky <msknjynt@gmail.com>
2022-08-30 16:01:56 +0530

    login page

---
+++ app/src/main/java/com/example/myloginpage/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myloginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };thread.start();
+    }
+}
---
+++ app/src/main/java/com/example/myloginpage/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.example.myloginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        TextView haveAccount = findViewById(R.id.have_account);
+        haveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
